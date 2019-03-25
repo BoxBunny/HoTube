c7f6fd22f3b3605de1794a801b910b5592ee9120
Jiho Han <mlpzaq123456@naver.com>
2019-03-25 16:02:32 +0900

    Middlewares
    Helmet
    Margan
    Cookie-Parser
    Body-Parser

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import logger from "morgan";
 import express from "express";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = 4000;
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send("Hello World from home");
 const handleProfile = (req, res) => res.send("You are on my profile");
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
 app.use(logger("dev"));
 app.listen(PORT, handleListening);
 app.get("/profile", handleProfile);
