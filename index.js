37693d9e1b70f9c0b44d4a95a9702c89d7e70cdd
Jiho Han <mlpzaq123456@naver.com>
2019-03-25 15:49:57 +0900

    morgan

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-//const express = require('express');
+import logger from "morgan";
 import express from "express";
-const app = express();
 
+const app = express();
 const PORT = 4000;
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send("Hello World from home");
 const handleProfile = (req, res) => res.send("You are on my profile");
-const betweenHome = (req, res, next) => {
-    console.log("I'm between");
-    next();
-}
+app.use(logger("dev"));
 app.listen(PORT, handleListening);
-app.use(betweenHome);
-app.get('/', betweenHome, handleHome);
-app.get("profile", handleProfile);
+app.get("/profile", handleProfile);
+app.get("/", handleHome);
