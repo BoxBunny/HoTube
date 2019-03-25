438f120d70a6fc10f5d014ec98b75e9e90364bb2
Jiho Han <mlpzaq123456@naver.com>
2019-03-25 16:37:35 +0900

    ExpressCore_Routing

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ import express from "express";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { userRouter } from "./router";
 
 const app = express();
-const PORT = 4000;
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+
 const handleHome = (req, res) => res.send("Hello World from home");
+
 const handleProfile = (req, res) => res.send("You are on my profile");
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(logger("dev"));
-app.listen(PORT, handleListening);
-app.get("/profile", handleProfile);
+
 app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.use("/user", userRouter);
+
+export default app;
