444d299c303a3aa6320fc9e8c7fe163d24a321b4
Jiho Han <mlpzaq123456@naver.com>
2019-03-25 15:16:50 +0900

    middlware_basic

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const PORT = 4000;
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send("Hello World from home");
 const handleProfile = (req, res) => res.send("You are on my profile");
-
+const betweenHome = (req, res, next) => {
+    console.log("I'm between");
+    next();
+}
 app.listen(PORT, handleListening);
-
-app.get('/', handleHome);
+app.use(betweenHome);
+app.get('/', betweenHome, handleHome);
 app.get("profile", handleProfile);
