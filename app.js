39827811a9b617c46c982e1cb033ccf92b636c41
Jiho Han <mlpzaq123456@naver.com>
2019-03-31 22:21:23 +0900

    pug_localVar

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(logger("dev"));
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
