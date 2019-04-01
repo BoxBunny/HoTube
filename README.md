caeff84b72c9f68dfcc257a6e0f1e86020c67b3f
Jiho Han <mlpzaq123456@naver.com>
2019-04-01 16:17:09 +0900

    pug_homeController_pt2

--- README.md
+++ README.md
@@ -25,9 +25,9 @@ Cloning YouTube with Vanilla and NodeJS for Full-Stack
 - [x] Login
 - [x] Search
 - [ ] User Detail
-- [ ] Edit Profile
-- [ ] Change Password
-- [ ] Upload
+- [x] Edit Profile
+- [x] Change Password
+- [x] Upload
 - [ ] Video Detail
-- [ ] Edit Video
+- [x] Edit Video
   
---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+      id: 100000,
+      title: "01 awesome",
+      description: "1번 비디오",
+      views: 100322,
+      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      creator: {
+        id: 121212,
+        name: "ComSenpai",
+        email: "Com@Senpai.com"
+      }
+    },
+    {
+      id: 200000,
+      title: "02 super",
+      description: "2번 비디오",
+      views: 123452,
+      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      creator: {
+        id: 212222,
+        name: "ComSenpai",
+        email: "Com@Senpai.com"
+      }
+    },
+    {
+      id: 300000,
+      title: "03 nice",
+      description: "3번 비디오",
+      views: 24,
+      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      creator: {
+        id: 332333,
+        name: "ComSenpai",
+        email: "Com@Senpai.com"
+      }
+    },
+    {
+      id: 400000,
+      title: "04 perfect",
+      description: "4번 비디오",
+      views: 24,
+      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      creator: {
+        id: 444222,
+        name: "ComSenpai",
+        email: "Com@Senpai.com"
+      }
+    }
+  ];
