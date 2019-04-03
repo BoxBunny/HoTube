2f50f8a72a94228eb623d0816f64705204acc807
Jiho Han <mlpzaq123456@naver.com>
2019-04-03 17:07:49 +0900

    mongodb_install

--- db.js
+++ db.js
@@ -1,54 +1,17 @@
-export const videos = [
-    {
-      id: 100000,
-      title: "01 awesome",
-      description: "1번 비디오",
-      views: 100322,
-      videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 121212,
-        name: "ComSenpai",
-        email: "Com@Senpai.com"
-      }
-    },
-    {
-      id: 200000,
-      title: "02 super",
-      description: "2번 비디오",
-      views: 123452,
-      videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 212222,
-        name: "ComSenpai",
-        email: "Com@Senpai.com"
-      }
-    },
-    {
-      id: 300000,
-      title: "03 nice",
-      description: "3번 비디오",
-      views: 5212,
-      videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 332333,
-        name: "ComSenpai",
-        email: "Com@Senpai.com"
-      }
-    },
-    {
-      id: 400000,
-      title: "04 perfect",
-      description: "4번 비디오",
-      views: 532,
-      videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 444222,
-        name: "ComSenpai",
-        email: "Com@Senpai.com"
-      }
-    }
-  ];
+import mongoose from "mongoose";
+
+monngoose.connect(
+  "mongodb://localhost:27017/ho-tube",
+  {
+    useNewUrlParser: true,
+    useFrindAndModify: false
+  }
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
