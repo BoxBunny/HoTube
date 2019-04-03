df0c6a16c983838b50b836f8c17341b38377b561
Jiho Han <mlpzaq123456@naver.com>
2019-04-03 15:44:12 +0900

    Log In and User Profile Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,12 +13,17 @@ export const postJoin = (req, res) => {
     res.render("join", { pageTitle: "Join" });
   } else {
     // To Do: Register User
-    // To Do: Log user in
+    // To Do: Log user in ``
     res.redirect(routes.home);
   }
 };
 
-export const login = (req, res) => res.render("login", { pageTitle: "Log In" });
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
 export const logout = (req, res) => res.render("logout", { pageTitle: "Log Out" });
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
