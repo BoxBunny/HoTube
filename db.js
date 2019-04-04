c1e51ac1b33e6c12e1909888067579d91e4609f6
Jiho Han <mlpzaq123456@naver.com>
2019-04-04 11:28:07 +0900

    mongo_dotenv

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-monngoose.connect(
-  "mongodb://localhost:27017/ho-tube",
+mongoose.connect(
+  process.env.MONGO_URL,
   {
     useNewUrlParser: true,
     useFrindAndModify: false
