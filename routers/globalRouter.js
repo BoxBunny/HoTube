5200d7348b02e761344e3705a54e952812277194
Jiho Han <mlpzaq123456@naver.com>
2019-04-11 22:53:24 +0900

    MongoStore and Middlewares
    modified

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -13,14 +13,14 @@ import { onlyPublic } from "..middlewares";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.join, onlyPublic);
-globalRouter.post(routes.join, onlyPublic, postLogin);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 
-globalRouter.get(routes.login, onlyPublic);
-globalRouter.post(routes.login, onlyPublic);
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.logout, onlyPublic);
+globalRouter.get(routes.logout, onlyPublic, logout);
 
 export default globalRouter;
