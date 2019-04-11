f6bf2b39bcfe2c39017b6499e644c4f6867b5e9c
Jiho Han <mlpzaq123456@naver.com>
2019-04-11 22:50:51 +0900

    MongoStore and Middlewares

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import passport from "passport";
+import mongoose from "mongoose";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 import "./passport";
+
 const app = express();
+const CokieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug");
@@ -25,10 +29,12 @@ app.use(
   session({
     secret: process.env.COOKIE_SECRET,
     resave: true,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: new CokieStore({ mongooseConnection: mongoose.connection })
   })
   //쿠키에 들어있는 세션ID를 암호화하기위해 무작위 문자열을 사용한다. 세션을 강제로 저장하게 한다. 초기화되지 않은 세션을 저장소에 저장한다.
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(localsMiddleware);
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -9,16 +9,18 @@ import {
   postLogin
 } from "../controllers/userController";
 
+import { onlyPublic } from "..middlewares";
+
 const globalRouter = express.Router();
 
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin, postLogin);
+globalRouter.get(routes.join, onlyPublic);
+globalRouter.post(routes.join, onlyPublic, postLogin);
 
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
+globalRouter.get(routes.login, onlyPublic);
+globalRouter.post(routes.login, onlyPublic);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.logout, onlyPublic);
 
 export default globalRouter;
